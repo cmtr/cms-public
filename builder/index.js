e72caa6813d1ab53e16c1ec69ccb30d73442af2f
cmtr <46787135+cmtr@users.noreply.github.com>
2021-07-10 15:31:26 +0200

    refactor to load data locally or via github

--- builder/index.js
+++ builder/index.js
@@ -1,58 +1,94 @@
+const fs = require('fs');
+const { promisify } = require('util');
 const _ = require('lodash');
 const fp = require('lodash/fp');
 const axois = require('axios');
 
 const rootUrl = 'https://raw.githubusercontent.com/cmtr/cms-public/main/content/index.json';
+const github = {
+	rootUrl: 'https://raw.githubusercontent.com/cmtr/cms-public/main/content/',
+	user: 'cmtr',
+	repository: 'cms-public',
+	branch: 'main'
+};
+
+const githubRoot = 'https://raw.githubusercontent.com/cmtr/cms-public/main/content/';
+const fileRoot = '/home/harald/Workspace/cms-public/content/';
+const index = 'index.json';
+
+
 
 // Helpers
-const resolve = (func) => (promise) => promise.then(func);
+const resolve = (func) => (promise) => promise.then(func).catch(console.log);
 const peek = (obj) => {
 	console.log(obj);
 	return obj;
-}
+};
 
 // Predicates
 const isObject = (obj) => typeof obj === 'object' && obj !== null;
 const isArray = Array.isArray;
 const isType = (type) => (obj) => isObject(obj) && obj.type && obj.type === type;
+const isURL = (str) => {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return pattern.test(str);
+}
+
 
 // Get Data
-const getJson = (url) => axois
+const getJsonFromUrl = (url) => axois
 	.get(url)
 	.then(fp.get('data'));
+
+const getJsonFromFile = (file) => promisify(fs.readFile)(file, 'utf-8')
+	.then(JSON.parse)
+
+const getJson = (root) => (path) => {
+	console.log(path);
+	return 	isURL(root) 
+	? getJsonFromUrl(root + path)
+	: getJsonFromFile(root + path).catch((err) => {
+		console.log('Error in file: ' + path);
+		console.log(JSON.parse(JSON.stringify(err)))
+		console.log()
+	});
+}
 
 const reduceEntriesToObject = (obj={}) => (arr) => arr
 	.reduce((acc, [key, value]) => _.set(acc, key, value), obj);
 
-const getSubtree = async (obj) => isType('map')(obj)
-		? getJson(obj.source)
+const getSubtree = (root) => async (obj) => isType('map')(obj)
+		? getJson(root)(obj.source)
 		: obj;
 
-const traverseThree = (func) => async (obj) => Promise
+const traverseThree = (root, func) => async (obj) => Promise
 	.all(Object
 		.entries(obj)
 		.map(([key, val]) => func(val)
 				.then(res => {
 					if (isObject(val)) 
-						return traverseThree(func)(res)
+						return traverseThree(root, func)(res)
 							.then((data) => [key, data]);
 					if (isArray(val))
 						return Promise
-							.all(res.map(traverseThree(func)))
+							.all(res.map(traverseThree(root, func)))
 							.then((res) => [key, res]);
 					return [key, res]
 				})))
-	.then(reduceEntriesToObject(obj))
+	.then(reduceEntriesToObject(obj));
 
 
-const build = fp.flow(
-	getJson,
-	resolve(traverseThree(getSubtree)),
+const build = (root) => fp.flow(
+	getJson(root),
+	resolve(traverseThree(root, getSubtree(root))),
 //	resolve(async (data) => _.get(data, 'about.team.team')),
 	resolve(console.log)
 );
 
-const root = build(rootUrl);
-
-
+const root = build(githubRoot)(index);
 	
